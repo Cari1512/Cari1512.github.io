2b66d00ffc9abd8df81ceabd06ff1d1bd3798f2e
Cari1512 <carianne.sauermann@hs-furtwangen.de>
2020-01-10 11:59:18 +0100

    A

--- Aufgabe_9/aufgabe_9.js
+++ Aufgabe_9/aufgabe_9.js
@@ -1,7 +1,50 @@
-var toDos = ["Go Home", "Drink Tea", "Sleep"];
+var arraytodo = [];
+var counter = "" + arraytodo.length;
 window.addEventListener("load", function () {
-    for (var index = 0; index < toDos.length; index++) {
-        document.querySelector("#toDos").innerHTML += "<p>" + toDos[index] + "<p>";
-    }
+    //Input
+    document.querySelector("#ADD").addEventListener("click", toDoInput);
 });
+//imput kriegen
+function toDoInput(e) {
+    e.preventDefault();
+    var inputValue = document.querySelector(".INPUT").value;
+    console.log(inputValue);
+    if (inputValue)
+        addTask(inputValue);
+    document.querySelector(".INPUT").value = "";
+}
+//Task dazu
+function addTask(inputValue) {
+    //neue Liste
+    var para = document.createElement("li");
+    para.innerText = inputValue; //para nimmt den Wert vom input Feld
+    para.classList.add("far", "fa-circle"); // ckeckbox wird vorne dran geklebt
+    var trash = document.createElement("button"); //button für müll
+    trash.classList.add("fa", "fa-trash-alt");
+    arraytodo.push(inputValue); //in array
+    console.log(arraytodo);
+    var counter = "" + arraytodo.length;
+    document.querySelector(".header").innerHTML = counter + " tasks";
+    para.appendChild(trash); //müll in para
+    document.querySelector("#toDos").appendChild(para); //Liste para in den wrap der to do liste
+    trash.addEventListener("click", Bin); // Müll funktion-->
+    para.addEventListener("click", Check); //Check funktion
+    function Check() {
+        if (para.classList.contains("fa-circle")) {
+            para.classList.remove("fa-circle");
+            para.classList.add("fa-check-circle");
+        }
+        else {
+            para.classList.remove("fa-check-circle");
+            para.classList.add("fa-circle");
+        }
+    }
+}
+function Bin() {
+    this.parentNode.remove();
+    console.log("removed");
+    arraytodo.pop();
+    var counter = "" + arraytodo.length;
+    document.querySelector(".header").innerHTML = counter + " tasks";
+}
 //# sourceMappingURL=aufgabe_9.js.map
--- Aufgabe_9/aufgabe_9.ts
+++ Aufgabe_9/aufgabe_9.ts
@@ -1,8 +1,56 @@
-var toDos: string [] = ["Go Home", "Drink Tea", "Sleep"];
+var arraytodo: string[] = [];
+var counter: string = "" + arraytodo.length;
+
 
 window.addEventListener("load", function (): void {
-
-    for (var index: number = 0; index < toDos.length; index++) {
-        document.querySelector("#toDos").innerHTML += "<p>" + toDos [index] + "<p>";
-}
+    //Input
+    document.querySelector("#ADD").addEventListener("click", toDoInput);
+    
+   
 });
+
+//imput kriegen
+function toDoInput(e: Event): void {
+    e.preventDefault();
+    var inputValue: string = (<HTMLInputElement>document.querySelector(".INPUT")).value;
+    console.log(inputValue);
+    if (inputValue)
+        addTask(inputValue);
+    (<HTMLInputElement>document.querySelector(".INPUT")).value = "";
+}
+//Task dazu
+function addTask(inputValue: string): void {
+    //neue Liste
+    var para: HTMLElement = document.createElement("li");
+    para.innerText = inputValue; //para nimmt den Wert vom input Feld
+    para.classList.add( "far", "fa-circle"); // ckeckbox wird vorne dran geklebt
+    var trash: HTMLButtonElement = document.createElement("button"); //button für müll
+    trash.classList.add("fa", "fa-trash-alt");
+    arraytodo.push(inputValue); //in array
+    console.log(arraytodo);
+    var counter: string = "" + arraytodo.length;
+    document.querySelector(".header").innerHTML = counter + " tasks";
+    para.appendChild(trash); //müll in para
+    document.querySelector("#toDos").appendChild(para); //Liste para in den wrap der to do liste
+    trash.addEventListener("click", Bin); // Müll funktion-->
+    para.addEventListener("click", Check); //Check funktion
+    function Check(): void {
+        if (para.classList.contains("fa-circle")) {
+            para.classList.remove("fa-circle");
+            para.classList.add("fa-check-circle");
+        }
+        else {
+            para.classList.remove("fa-check-circle");
+            para.classList.add("fa-circle");
+        }
+    }
+    
+}
+
+function Bin(): void {
+    this.parentNode.remove();
+    console.log("removed");
+    arraytodo.pop();
+    var counter: string = "" + arraytodo.length;
+    document.querySelector(".header").innerHTML = counter + " tasks";
+}
