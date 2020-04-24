0f190d3711400bcb5a801814a7a2eff28eb6b446
Cari1512 <56436935+Cari1512@users.noreply.github.com>
2020-04-24 14:04:21 +0200

    Eia1

---
+++ Playground.js
@@ -0,0 +1,3 @@
+var s = { "zahl": 7, "wahr": true, text: "Hallo" };
+console.log(s.text);
+//# sourceMappingURL=Playground.js.map
---
+++ Playground.ts
@@ -0,0 +1,2 @@
+let s = {"zahl": 7, "wahr": true, text: "Hallo"}
+console.log(s.text);
